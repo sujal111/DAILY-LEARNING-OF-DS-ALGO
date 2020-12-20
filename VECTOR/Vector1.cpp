346cfb5ebaa0bcd93158efd466ad1023d6eec771
sujal111 <62564807+sujal111@users.noreply.github.com>
2020-12-20 19:02:58 +0530

    Add files via upload

---
+++ VECTOR/Vector1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v1;
+    for(int i=1;i<5;i++)
+    v1.push_back();
+    cout<<"Output of begin and end is:"
+    for(auto i=v1.begin();i!=v1.end();i++)
+    cout << *i << " "; 
+}
---
+++ VECTOR/Vector2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v1;
+     for (int i = 1; i <= 10; i++) 
+    v1.push_back();
+    cout << "Size : " << g1.size();
+
+}
